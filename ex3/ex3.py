5d3c6abdfaf8f53386e28b755db2ebf5c149b85c
Jonathan Wood <jonathankimballwood@gmail.com>
2015-01-07 15:20:35 -0700

    modified ex3.py & deleted coding_campus_excercises directory

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -1,4 +1,10 @@
-float(user_input) = input("Please enter a speed in miles/hour: ")
+def conver_to_float(user_input):
+	float_num = float(user_input)
+	return float_num
+
+def user_output():
+	user_input = input("Please enter a speed in miles/hour: ")
+
 print("Original speed in mph is: %s" % user_input)
 
 temporary_baleycorn_per_day_conversion_varable = 1609.34
